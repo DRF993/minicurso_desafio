1e6d3a46bd08a5464c2d18b1f70cd813ce4e36df
DRF993 <drfjava@gmail.com>
2022-01-16 23:27:05 -0300

    Finalização do código do arquivo JavaScript

---
+++ script.js
@@ -0,0 +1,61 @@
+
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
+
+
+gerarValorAletorio = () => {
+
+   for (var i = 0; i < 2; i++) {
+        
+       var qt = Math.floor(Math.random() * 671);
+
+       return qt;
+
+    }
+}
+
+
+
+pegarPersonagem = () => {
+
+     let numeroAleatorio = gerarValorAletorio();
+
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    
+    
+     }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = traduzirCondicao(data);
+    
+    });
+}
+
+
+botao.onclick = pegarPersonagem;
+
+
+
